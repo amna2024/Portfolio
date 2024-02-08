e9aebf68f5e814ba427f97570d42caf18eb3f08c
Amna <151729717+amna2024@users.noreply.github.com>
2024-02-08 11:48:57 +0100

    Create script.ps1

---
+++ powershell/change administrator into new user/script.ps1
@@ -0,0 +1,10 @@
+﻿
+#Change the Administrator to som user
+Get-LocalUser -Name "Volvo-admin" | Rename-LocalUser -NewName "xyz" -Confirm
+Set-LocalUser -Name "xyz" -FullName "xyz" -Confirm
+
+#change the password
+Set-LocalUser -Name "xyz" -Password (ConvertTo-SecureString -AsPlainText "xyz123456789" -Force)
+
+#change computer name 
+Rename-Computer -ComputerName DESKTOP-TD7H6G5 -NewName "comp-01" -Force -Restart

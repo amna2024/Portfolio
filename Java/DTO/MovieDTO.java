7b724dd38447e0e17c95fec50706f1e13cb87df1
Amna <151729717+amna2024@users.noreply.github.com>
2024-10-29 15:54:33 +0100

    Add files via upload

---
+++ Java/DTO/MovieDTO.java
@@ -0,0 +1,11 @@
+package com.example.todo_application.dto;
+import com.example.todo_application.model.Movie; // Import the Movie entity
+
+public class MovieDTO extends Movie {
+
+
+    public MovieDTO() {
+
+    }
+
+}
---
+++ Java/DTO/UserDTO.java
@@ -0,0 +1,12 @@
+package com.example.todo_application.dto;
+
+import com.example.todo_application.model.User;
+
+public class UserDTO  extends User{
+
+    public UserDTO(){
+
+
+    }
+
+}
